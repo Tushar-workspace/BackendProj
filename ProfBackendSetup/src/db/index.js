6198099068b9d5278b2a3abaf50f582c4fff8110
Tushar-workspace <tusharsarkar.workspace@gmail.com>
2025-07-18 12:23:38 +0530

    connectivity

--- ProfBackendSetup/src/db/index.js
+++ ProfBackendSetup/src/db/index.js
@@ -4,6 +4,8 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {  // always try to wrap the DB connection using try catch method 
     try {
         const connect = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        
+                //When await is encountered, it pauses the execution of the async function until the Promise it's waiting on (in this case, the promise returned by mongoose.connect()) either resolves successfully or rejects with an error.
 
                 // mongoose.connect() performs network I/O to establish a connection to the MongoDB Atlas cluster.
 
@@ -15,13 +17,13 @@ const connectDB = async () => {  // always try to wrap the DB connection using t
 
 
 
-        console.log(`\n MongoDB connected !! DB HOST: ${connect.connection.host}`);
+        console.log(`\n MongoDB connected !! DB HOST in src/db/index : ${connect.connection.host}`);
     } catch (error) {
 
-        console.log("MongoDB Connection error : ",error);
+        console.log("MongoDB Connection error in src/db/index : ",error);
         process.exit(1);
 
-            // process.exit(code) ends the Node.js process immediately.
+            // process.exit(code) ends the Node.js process immediately, here process is the reference which is happening in the backend of the node  .
 
             // code = 0 means success.
 
--- ProfBackendSetup/src/index.js
+++ ProfBackendSetup/src/index.js
@@ -3,43 +3,35 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
+app.on("error in Express App", (error) => {
+  console.log("ERR: ", error);
+  throw error;
+});
 
 dotenv.config({
-    path : "./env"
-})
+  path: "./.env", //it is used for reloading the values which is being used in the project which is stored in the .env file
+});
 
-
-
-connectDB()  // it returning a promise that it will return something,that's why we r using catch n throw
-.then(
-    // before DB connectivity was done , now server has to listen the db to any specific port which can be taken from env or specify here
-    ()=> {
-        app.listen(process.env.PORT || 8080 , ()=> {
-            console.log(`server is running on the port : ${process.env.PORT}`);
-            
-        })
-    }  
-)
-.catch( (err) => {
-    console.log("MongoDB connection failed !!! : ",err);
-    
-})
-
-
-
-
-
-
-
+connectDB() // it returning a promise that it will return something,that's why we r using catch n throw
+  .then(
+    // after DB connectivity, now server has to listen the db to any specific port which can be taken from env or specify here
+    () => {
+      app.listen(process.env.PORT || 8080, () => {
+        console.log(
+          ` server is running on the port in utils/index : ${process.env.PORT}`,
+        );
+      });
+    },
+  )
+  .catch((err) => {
+    console.log("MongoDB connection failed in utils/index!!! : ", err);
+  });
 
 // First approach for DB connectivity
-
-
 
 // import express from "express";
 
 // const app = express();
-
 
 // ;( async () => {
 //     try {
@@ -52,7 +44,7 @@ connectDB()  // it returning a promise that it will return something,that's why 
 
 //         app.listen(process.env.PORT, () => {
 //             console.log(`App is listening on the port ${process.env.PORT}`);
-            
+
 //         } )
 //     } catch (error) {
 //         console.error("Error: ", error)
